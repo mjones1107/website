78623981d4b1ef3106e264e0f213250764751f35
MJTide <45186909+mjones1107@users.noreply.github.com>
2019-12-22 14:21:36 -0500

    contact form

--- contactform/contactform.php
+++ contactform/contactform.php
@@ -8,7 +8,7 @@
 /***************** Configuration *****************/
 
   // Replace with your real receiving email address
-  $contact_email_to = "contact@example.com";
+  $contact_email_to = "mjones1107@gmail.com";
 
   // Title prefixes
   $subject_title = "Contat Form Message:";
